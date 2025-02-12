Trying to fix divergence in learning: no exploding gradient, no vanising gradient, Seems to be a learning rate, updating target Q-net that needs to be configured to work

DQN/agent.py
@@ -43,7 +43,9 @@ class CNN_Q_fun(nn.Module):
         )
 
         self.final_function =nn.Sequential(
-            nn.Linear(32 + 12,12),
+            nn.Linear(32 + 4,32),
+            nn.ReLU(),
+            nn.Linear(32,12),
             nn.ReLU(),
             nn.Linear(12,self.n_actions)
         ) 
@@ -51,7 +53,6 @@ class CNN_Q_fun(nn.Module):
     def forward(self,x,y):
         
         x = self.CNN_function(x)
-        y = self.global_function(y)
         
         combined = torch.cat((x,y),dim=-1)
         result = self.final_function(combined)
@@ -133,7 +134,7 @@ class CNN_Maze_Agents:
 
     def add_wrappers(self, env):
         """Add wrappers into the enviroment"""
-        env = rw.maze_runner_rewards(env)
+        #env = rw.maze_runner_rewards(env)
         return env
 
     def get_action(self,env,num_agents,state,epsilon=0):

DQN/training.py
@@ -74,7 +74,7 @@ class Maze_Training:
         self.optimizer = torch.optim.Adam(self.agents.Q_fun.parameters(),lr=self.lr)
 
         # Schedular to lower the lr
-        self.scheduler = StepLR(self.optimizer,step_size=100,gamma=0.5)
+        self.scheduler = StepLR(self.optimizer,step_size=30,gamma=0.1)
 
         # results from training
         self.losses = []
@@ -127,6 +127,7 @@ class Maze_Training:
         reward_t = torch.tensor(np.array(reward),dtype=torch.float32,device = device.DEVICE)
         terminated_t = torch.tensor(np.array(terminated),dtype = torch.int64,device= device.DEVICE)
 
+
         return local_st,global_st,action_t,next_local_st,next_global_st,reward_t, terminated_t
 
     def compute_loss(self,batch_size, gamma=0.99,lambda_entropy = 0.01):
@@ -141,24 +142,35 @@ class Maze_Training:
         # Get the states and actions
         local_s,global_s, actions,next_local_s,next_global_s, rewards, terminated = self.sample_replay(batch_size)
         
-        # get our policy q-values 
+        # We need to find $Q^*(s,a) \approx r + \gamma * Q(s', max_{a'} Q'(s',a'))
+        # Where (s,a,r,s') is from the replay buffer, Q is the policy net, Q' is the target net
+
+
+        # get Q(s,a)
         q_values = self.agents.Q_fun(local_s,global_s)
         selected_q_values = q_values.gather(1,actions.unsqueeze(1)).squeeze(1)
-        #print('q_values',torch.isinf(q_values).any())
-        with torch.no_grad():
-            # Next, we calculate the the target q-values which are maximized
-            next_q_values = self.target_Q_net(next_local_s,next_global_s).max(1)[0]
 
+        with torch.no_grad():
+
+            next_actions = self.agents.Q_fun(next_local_s, next_global_s).argmax(1, keepdim=True)
+
+            # Next, we calculate the the target q-values which are maximized
+            next_q_values = self.target_Q_net(next_local_s,next_global_s).gather(1,next_actions).squeeze(1)
+            
             # Then we calculate the estimated Bellmann expectation operator
             target = rewards + gamma*next_q_values * (1-terminated.float())
 
         # Compute the loss between the Bellman expectation operator and the policy q-values
         loss = self.loss_fun(selected_q_values,target)
-
+        #print(f"Reward mean: {rewards.mean().item()}, std: {rewards.std().item()}")
+        #print(f"Q_target mean: {target.mean().item()}, std: {target.std().item()}")
+        #print(f"Q_policy mean: {selected_q_values.mean().item()}, std: {selected_q_values.std().item()}")
+        
         # Now, for the entropy, we want to look at $pi(a|s), the probablity of action a given state s
         # and compute its entropy over all the actions, and take the average over the states.
         action_probs = torch.softmax(q_values,dim=1)
         entropy = -torch.sum(action_probs * torch.log(action_probs + 1e-6),dim=1)
+        
         #print('entropy',torch.isinf(entropy).any())
         loss -=lambda_entropy * entropy.mean()
         #print('Loss',torch.isinf(loss).any())
@@ -180,6 +192,9 @@ class Maze_Training:
 
         # discount factor
         gamma = 0.99
+        
+        # soft update
+        tau = 0.01
 
         # This is used to change the maze enviroment to train in, so that the agent 
         # does not hyper learn one maze
@@ -252,7 +267,7 @@ class Maze_Training:
                 # iterations of the enviroment, enact a soft update of the target Q-net
                 if len(self.replay_buffer)>=10000 and update_target % self.update_factor ==0:
                     #self.target_Q_net.load_state_dict(self.agent.Q_fun.state_dict())
-                    self.soft_update(tau=0.01)
+                    self.soft_update(tau=tau)
 
                 update_target+=1
 
@@ -262,16 +277,20 @@ class Maze_Training:
                 if len(self.replay_buffer)>=10000 and update_target % 4 == 0:
 
                     self.optimizer.zero_grad()
-                    loss,action_prob = self.compute_loss(batch_size=64,lambda_entropy=lambda_entropy)
+                    loss,action_prob = self.compute_loss(batch_size=64,gamma=gamma,lambda_entropy=lambda_entropy)
                     self.losses.append(loss.detach().cpu().numpy())
                     loss.backward()
+
+                    #for param in self.agents.Q_fun.parameters():
+                    #    if param.grad is not None:
+                    #        print(param.grad.abs().mean())
 
                     # Implement gradient cutoffs
                     nn.utils.clip_grad_norm_(self.agents.Q_fun.parameters(),1)
 
                     self.optimizer.step()
                     self.scheduler.step()
-                    if update_target % 100 ==0:
+                    if update_target % 1000 ==0:
                         print(f'episode {ep} with loss {loss}')
                     #if update_target % 200 == 0:
                         #print(f'action|state distribution: {action_prob.mean(0)}')

Maze_env/wrappers/rewards.py
@@ -9,6 +9,7 @@ class maze_runner_rewards(Wrapper):
         
         self.agents_dist = {}
         self.agents_recent_loc = {}
+        self.agents_past = {}
 
         self.reward_mean = 0
         self.reward_var = 1
@@ -34,11 +35,24 @@ class maze_runner_rewards(Wrapper):
     def step(self,action):
         new_obs, reward, terminated, truncated, info = super().step(action)
     
-        for k in range(len(reward)):
+        for k in range(info['n_agents']):
             self.agents_dist[f'agent_{k}'].append(info[f'agent_{k}']['man_dist'])
-            self.agents_recent_loc[f'agent_{k}'].append(info[f'agent_{k}']['pos'])
 
-            if len(set(self.agents_recent_loc[f'agent_{k}']))<2:
+            pos = info[f'agent_{k}']['pos']
+            self.agents_recent_loc[f'agent_{k}'].append(pos)
+
+            # Punish for going to the same place it has already visted
+            if pos not in self.agents_past:
+                self.agents_past[f'agent_{k}'].add(pos)
+            else:
+                reward[k]-=0.25
+
+            reward[k] = np.clip(reward[k],-1,1)
+            
+
+
+
+            if len(set(self.agents_recent_loc[f'agent_{k}']))<3:
                 reward[k]-=0.2
             else:
                 reward[k]+=0.1
@@ -48,8 +62,8 @@ class maze_runner_rewards(Wrapper):
             #else:
             #    reward[k]-=0.5
 
-            if not info[f'agent_{k}']['done'] and truncated:
-                reward[k] -=info['timer']
+            #if not info[f'agent_{k}']['done'] and truncated:
+                #reward[k] -=info['timer']
         
         #reward = self.normalize_rewards(reward)
         
@@ -62,6 +76,7 @@ class maze_runner_rewards(Wrapper):
         for a in range(info['n_agents']):
             self.agents_dist[f'agent_{a}'] = deque([info[f'agent_{a}']['man_dist']],maxlen=2)
             self.agents_recent_loc[f'agent_{a}'] = deque([info[f'agent_{a}']['pos']],maxlen=3)
+            self.agents_past[f'agent_{a}'] = {info[f'agent_{a}']['pos']}
 
         
 